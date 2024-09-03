d3966895df89c4928dd942cb0b13034fb0ec083c
saravanansritherkumar <saravanansritherkumar28@gmail.com>
2024-09-03 14:39:28 +0530

    NEW-PROJECT

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+export default function Header() {
+  return (
+    <div id='header'>
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+        <p>Mail</p>
+    </div>
+  )
+}
