7be31069908b97fe5e527397964cda0919ba25f0
nidarsan <Sannidarsan@gmail.com>
2024-09-10 19:54:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer"
+import OURGALERY  from './Components/OURGALERY'
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <OURGALERY/>
         <Footer/>
         
         
