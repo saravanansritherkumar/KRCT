4e7447b662816a6418e5a6fea17b4db64cebf443
saravanansritherkumar <saravanansritherkumar28@gmail.com>
2024-09-11 19:10:02 +0530

    updated-3

--- src/Components/OURGALERY.js
+++ src/Components/OURGALERY.js
@@ -11,29 +11,23 @@ import '../Components/OURGALLERY.css'
 
 const OURGALERY = () => {
   return (
-    
-<div className='mainscroll' >
+    <>
+      <center>
 
-<h4 className='galleryheading'>OUR GALLERY</h4>
+        <div className='mainscroll' >
+          <h4 className='galleryheading'>OUR GALLERY</h4>
+          <div className='col-lg-3' id='scroll' >
+            <img src={img1} id='im' height={200} width={220}></img>
+            <img src={img2} id='im' height={200} width={300} ></img>
+            <img src={img3} id='im' height={200} width={300} ></img>
+            <img src={img4} id='im' height={200} width={400} ></img>
+            <img src={img5} id='im' height={200} width={400} ></img>
+            <img src={img6} id='im' height={200} width={400} ></img>
+          </div>
+        </div>
+      </center>
 
-<div  className='col-lg-3' id='scroll' > 
-
-
-    
-   <img src={img1} id='im' height={200} width={220}></img>
-  <img src={img2} id='im' height={200} width={300} ></img>
-  <img src={img3} id='im' height={200} width={300} ></img>
-  <img src={img4} id='im' height={200} width={400} ></img>
-  <img src={img5} id='im' height={200} width={400} ></img>
-  <img src={img6} id='im' height={200} width={400} ></img>
-  </div>
-
-
-  
-
-
-        
-    </div>
+    </>
   )
 }
 
