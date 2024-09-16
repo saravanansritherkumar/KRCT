59725f5955d8e5dd32850e7660bce2371939a53b
saravanansritherkumar <saravanansritherkumar28@gmail.com>
2024-09-16 09:48:01 +0530

    full-updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Half from './Half'
 import Half1 from './Half1'
+import Border from './Components/Border'
 
 const App = () => {
   return (
     <div>
-      {/* <Half1/> */}
+      <Half1/>
       <Half/>
     </div>
   )
